4849b5b620511c4d21d2a322960b0a1fc624bdbc
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-29 21:02:21 +0000

    Deploying to gh-pages from @ irinasokolenko/goit-js-hw-10@f6d1aa268e2ba1dcac11e1a78925a8620303d572 🚀

--- index.4206bc95.js
+++
@@ -1,2 +0,0 @@
-axios.defaults.common["x-api-key"]="live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4IbSKDrBp2Z",axios.get("https://api.thecatapi.com/v1/images/search/breeds").then((a=>a.data)).then(consol.log);
-//# sourceMappingURL=index.4206bc95.js.map
--- index.b7e78ab7.js
+++
@@ -1,2 +0,0 @@
-!function(){var a="https://api.thecatapi.com/v1/images/search";axios.defaults.common["x-api-key"]="live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4IbSKDrBp2Z",axios.get("".concat(a,"/breeds")).then((function(a){return a.data})).then(consol.log)}();
-//# sourceMappingURL=index.b7e78ab7.js.map
