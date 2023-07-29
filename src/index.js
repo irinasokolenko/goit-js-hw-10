f6d1aa268e2ba1dcac11e1a78925a8620303d572
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-30 00:02:00 +0300

    hw10js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
-axios.defaults.common['x-api-key'] = 'live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4IbSKDrBp2Z';
+axios.defaults.common['x-api-key'] = live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4IbSKDrBp2Z;
 export function fetchBreeds() {
     return axios.get(`${BASE_URL}/breeds`).then(Response => Response.data);
 }
@@ -11,5 +12,5 @@ export function fetchCatByBreed(breedId){
     });
 }
 fetchBreeds().then(consol.log);
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+// import SlimSelect from 'slim-select';
+// import 'slim-select/dist/slimselect.css';
