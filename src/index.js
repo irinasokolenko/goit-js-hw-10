7c1782b54292eefc69fa7ce9a9402118370d14b6
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-29 23:34:58 +0300

    hw10js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
 axios.defaults.common['x-api-key'] = 'live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4IbSKDrBp2Z';
 export function fetchBreeds() {
-    return axios.get('https://api.thecatapi.com/v1/breeds');
+    return axios.get(`${BASE_URL}/breeds`).then(Response => Response.data);
 }
 export function fetchCatByBreed(breedId){
-    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids= ${breedId}`);
+    return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(Response => {
+        return Response.data;
+    })
 }
