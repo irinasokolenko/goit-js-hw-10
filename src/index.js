c4c55c22b76904a9216a472c34ab6773971c61f2
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-30 00:05:09 +0300

    hw10js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
-axios.defaults.common['x-api-key'] = live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4IbSKDrBp2Z;
+axios.defaults.common['x-api-key'] = 'live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4';
 export function fetchBreeds() {
     return axios.get(`${BASE_URL}/breeds`).then(Response => Response.data);
 }
