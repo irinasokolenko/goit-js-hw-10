9fe9075b5b743cf461acf597575ca5328550ca3d
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-30 20:03:36 +0000

    Deploying to gh-pages from @ irinasokolenko/goit-js-hw-10@b3f84d7a3ef31987df5c2ec4fec936fa3e2e5f75 🚀

--- index.98500cde.js
+++
@@ -1,2 +0,0 @@
-!function(){var a="https://api.thecatapi.com/v1/images/search";axios.defaults.common["x-api-key"]="live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4",axios.get("".concat(a,"/breeds")).then((function(a){return a.data})).then(consol.log)}();
-//# sourceMappingURL=index.98500cde.js.map
--- index.aea60c87.js
+++
@@ -1,2 +0,0 @@
-axios.defaults.common["x-api-key"]="live_0OHVSrZmYOum5F9rOfmcgMp6UIbUTa5AQtDZVhO6MZ85hNASRodSA4",axios.get("https://api.thecatapi.com/v1/images/search/breeds").then((a=>a.data)).then(consol.log);
-//# sourceMappingURL=index.aea60c87.js.map
