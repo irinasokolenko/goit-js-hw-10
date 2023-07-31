789422c97b8649a96bdeb9fc24ba7cf110e82f3e
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-31 15:49:32 +0300

    hw10js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 import './styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select'
-import 'slim-select/dist/slimselect.css';
+const new SlimSelect({
+    select: '#selectElement'
+  });
 
 const ref = {
     selector: document.querySelector('.breed-select'),
